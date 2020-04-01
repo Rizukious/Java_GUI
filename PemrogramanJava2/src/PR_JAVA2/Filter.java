1f3945adf89eed1c1814c06bf72de391383b937a
Rizukious <52876118+Rizukious@users.noreply.github.com>
2020-04-02 02:15:07 +0700

    Upload Project Pemrograman Java2

---
+++ PemrogramanJava2/src/PR_JAVA2/Filter.java
@@ -0,0 +1,31 @@
+package PR_JAVA2;
+
+
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rizky Wahyudi
+ */
+public class Filter {
+    public static void digitFilter(KeyEvent evt){
+        char input = evt.getKeyChar();
+        if (! ((Character.isDigit(input)) ||
+                (input == KeyEvent.VK_BACK_SPACE) ||
+                    (input == KeyEvent.VK_DELETE))) {
+            
+            new JFrame().getToolkit().beep();
+            JOptionPane.showMessageDialog(null, "Tolong Masukkan hanya angka!!");
+            
+            evt.consume();
+        }
+    } 
+}
---
+++ PemrogramanJava2/src/pemrogramanjava2/JFrameBeraksi.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pemrogramanjava2;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Rizky Wahyudi
+ */
+public class JFrameBeraksi {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GUI App");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+    }
+}
